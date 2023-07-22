5fb634116acea8d543c69ea51b43b2dc54d3771a
ahaing <ahahaing@gmail.com>
2023-07-22 16:19:22 +0800

    卡關了ㄟ

---
+++ Lec009.py
@@ -0,0 +1,30 @@
+import requests
+
+def download_weather():
+    url ='https://opendata.cwb.gov.tw/fileapi/v1/opendataapi/F-C0032-007?Authorization=rdec-key-123-45678-011121314&format=JSON'
+
+    responseR = requests.get(url)
+    if responseR.status_code == 200:
+        print("下載成功")
+        weather = responseR.json()
+        return weather
+    else:
+        print("下載失敗")
+        return False
+
+
+def main():
+    print("main function")
+    # 下載json
+    weather = download_weather()
+    if weather != False:
+        print("下載完畢")
+    else:
+        print("應用程式下載失敗")
+        return
+
+
+if __name__ == "__main__":
+    main()  
+
+
